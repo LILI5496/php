8bd2ca9d554128bbaa8563c5c664786e03b76dba
LILI <18845593722@163.com>
2017-01-12 13:42:50 +0800

    php del

---
+++ 假期1/db/del.php
@@ -0,0 +1,15 @@
+<?php
+include "conn.php";
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "delete from blog where wid = '$id'";
+//    echo $sql;
+//    die();
+    $query = mysqli_query($link,$sql);
+
+    if ($query){
+        header('location:index.php');
+    }
+}
+?>
+
